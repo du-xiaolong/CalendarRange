016459133b0b0c5023cb041bebf30b004e7079ba
duxiaolong <du_xiaolong@hotmail.com>
2023-10-10 16:55:21 +0800

    bug fix

--- calendarRange/build.gradle.kts
+++ calendarRange/build.gradle.kts
@@ -3,7 +3,16 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
-group = "com.github.du-xiaolong"
+val GROUP_ID = "com.github.du-xiaolong"
+val ARTIFACT_ID = "CalendarRange"
+val VERSION = latestGitTag().ifEmpty { "1.0.0-SNAPSHOT" }
+
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return  process.inputStream.bufferedReader().use {bufferedReader ->
+        bufferedReader.readText().trim()
+    }
+}
 
 android {
     namespace = "com.dxl.calendarrange"
@@ -35,6 +44,21 @@ android {
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+publishing { // 发布配置
+    publications { // 发布的内容
+        register<MavenPublication>("release") { // 注册一个名字为 release 的发布内容
+            groupId = GROUP_ID
+            artifactId = ARTIFACT_ID
+            version = VERSION
+
+            afterEvaluate { // 在所有的配置都完成之后执行
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
     }
 }
 
